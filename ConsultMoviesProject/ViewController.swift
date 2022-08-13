125a03325006e4447dc02ce99307e9442eb6a46e
Cas9Diego <DaCastro@outlook.com>
2022-08-13 15:46:36 -0500

    Initial Commit

---
+++ ConsultMoviesProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConsultMoviesProject
+//
+//  Created by Diego Castro on 13/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
